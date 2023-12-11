153d5d963efadd45434e5aae627820156e05ca28
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-11 19:57:06 +0100

    Update test.asm

--- test.asm
+++ test.asm
@@ -1,5 +1,4 @@
 !cpu 6502
-!to "build/hello.prg",cbm
 * = $0801                               ; BASIC starts at #2049
 !byte $0d,$08,$dc,$07,$9e,$20,$34,$39   ; BASIC to load $c000
 !byte $31,$35,$32,$00,$00,$00           ; inserts BASIC line: 2012 SYS 49152
