fda1d5b69e116387991e35859f7d0e5cc99cc938
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-11 19:40:47 +0100

    Update test.asm

--- test.asm
+++ test.asm
@@ -1,12 +1,11 @@
 * = $0801  ; Set the start address
 
 ; BASIC loader to run the machine code
-!byte $0C, $08, $0A, $00, $9E, $20, $33, $32, $37, $31, $00, $00, $00
+!byte $0C, $08, $0A, $00, $9E, $32, $30, $38, $31, $34, $00, $00, $00
 
 * = $0814  ; Start address for the machine code
 
 ; Clear screen code
-clearScreen:
     lda #$00
     sta $d020   ; Set the border color to black
     sta $d021   ; Set the background color to black
@@ -24,8 +23,7 @@ clearLoop:
 
 ; Entry point
 start:
-    jsr clearScreen  ; Clear the screen
-    jmp start        ; Infinite loop to prevent falling into BASIC
+    jmp clearScreen  ; Clear the screen
 
 ; Unused memory for padding
 * = $0900
