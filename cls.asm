c2d1ee81bd21008461788c84ef34a8b8126cc7ea
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-11 19:49:41 +0100

    Update cls.asm

--- cls.asm
+++ cls.asm
@@ -1,13 +1,9 @@
-           * = $1000
+*=$0801
 
-           lda #$00      ; Put the value 0 in accumulator
-           sta $d020     ; Put value of acc in $d020
-           sta $d021     ; Put value of acc in $d021
-           tax           ; Put value of acc in x reg
-           lda #$20      ; Put the value $20 in acc
-clrloop:   sta $0400,x   ; Put value of acc in $0400 + value in x reg
-           sta $0500,x   
-           sta $0600,x   
-           sta $0700,x
-           dex            ; Decrement value in x reg
-           bne clrloop    ; If not zero, branch to clrloop
+ byte $0d,$08,$d9,$07,$9e,$20,$34,$39
+ byte $31,$35,$32,$00,$00,$00
+
+*=$c000
+
+ inc $d020
+ jmp $c000
