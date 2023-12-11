1d90ac30ce366d3d2c9106535901637a2a736ddf
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-11 19:22:09 +0100

    Update starfield.asm

--- starfield.asm
+++ starfield.asm
@@ -3,16 +3,32 @@
 ; BASIC loader to run the program
 !byte $0C, $08, $0A, $00, $9E, $20, $30, $36, $38, $30, $00, $00, $00
 
-* = $080D  ; Start address for the actual code
+* = $0814  ; Start address for the code
 
 ; Variables
 starX = $40
 starY = $41
 delay = $42
 
-* = $0814  ; Start address for the code
+; Clear screen code
+clearScreen:
+    lda #$00     ; Set the border color to black
+    sta $d020
+    sta $d021
 
-; Starfield simulation code
+    ldx #0       ; Initialize X for the screen clear loop
+
+clearLoop:
+    lda #$20     ; Set the character to SPACE
+    sta $0400, x ; Set the pixel in the screen memory
+    sta $0500, x
+    sta $0600, x
+    sta $0700, x
+    inx          ; Increment X
+    cpx #64      ; Check if we have cleared 256 characters (64 * 4)
+    bcc clearLoop
+
+; Starfield simulation code starts here
 mainLoop:
     lda #0      ; Initialize X position (horizontal)
     sta starX
@@ -64,7 +80,8 @@ init:
 
 ; Entry point
 start:
-    jsr init
+    jsr clearScreen  ; Clear the screen to black and SPACE characters
+    jsr init     
     jmp mainLoop
 
 ; Unused memory for padding
