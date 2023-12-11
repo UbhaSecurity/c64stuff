1cf0aff955b67751beb0df556e1a1f689a8c0a71
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-11 16:26:57 +0100

    Update starfield.asm

--- starfield.asm
+++ starfield.asm
@@ -1,29 +1,17 @@
 ; Simple Star Field Simulation for Commodore 64
 ; Press SPACE to exit
 
-        ; Memory locations for screen memory and color memory
-SCREEN  = $0400
-COLORRAM = $D800
-
-        ; Character set for stars (a simple dot)
-STAR_CHAR = $2E
-
-        ; Colors (white on black background)
-WHITE   = $01
-BLACK   = $00
-
-        ; Start address
-        !to $0801
+        .org $0801
 
         ; BASIC header
-        .byte $0C, $08, "STARFIELD", $00
+        .byte $0C, $08, $08, $00, $9E, $32, $30, $36, $31, $32, $00
 
         ; Machine code
-        !to $080D
+        .org $080D
 
-start   lda #BLACK         ; Set background color to black
+start   lda #$00          ; Initialize background color to black
         sta $D021
-        lda #STAR_CHAR      ; Set character for stars
+        lda #$2E          ; Set character for stars
         sta $D018
 
 main    jsr init          ; Initialize star field
@@ -33,42 +21,41 @@ loop    jsr updateStars   ; Update star positions
         jmp loop          ; Repeat
 
 ; Initialize the star field
-init    ldx #0            ; Clear screen memory
-clear   lda #32           ; Space character
-        sta SCREEN, x
-        sta COLORRAM, x
+init    ldx #$00         ; Clear screen memory and color memory
+clear   lda #$20
+        sta $0400, x
+        sta $D800, x
         inx
-        cpx #40           ; Check if we've cleared the entire screen
         bne clear
         rts
 
 ; Update star positions
 updateStars
-        ldx #0
+        ldx #$00
 updateLoop
-        lda $d012          ; Get raster line (vertical blank)
-        cmp #129           ; Check if in the lower half of the screen
-        bcc skipUpdate     ; If not, skip the update
-        lda $d020, x       ; Get the current X position of the star
+        lda $D012          ; Get raster line (vertical blank)
+        cmp #$81           ; Check if in the lower half of the screen
+        bcc .skipUpdate    ; If not, skip the update
+        lda $D020, x       ; Get the current X position of the star
         clc
-        adc #1             ; Move star to the right
-        sta $d020, x
-skipUpdate
+        adc #$01           ; Move star to the right
+        sta $D020, x
+.skipUpdate
         inx
-        cpx #8             ; Check if we've updated all 8 stars
+        cpx #$08           ; Check if we've updated all 8 stars
         bne updateLoop
         rts
 
 ; Draw stars on the screen
 drawStars
-        ldx #0
+        ldx #$00
 drawLoop
-        lda $d020, x       ; Get the X position of the star
+        lda $D020, x       ; Get the X position of the star
         sta $0400, x       ; Set the star position on the screen
-        lda #WHITE         ; Set star color to white
+        lda #$01           ; Set star color to white
         sta $D800, x       ; Set the star color in color memory
         inx
-        cpx #8             ; Check if we've drawn all 8 stars
+        cpx #$08           ; Check if we've drawn all 8 stars
         bne drawLoop
         rts
 
@@ -79,19 +66,3 @@ waitLoop
         cmp $D012           ; Wait until it's different from the current value
         beq waitLoop
         rts
-
-; IRQ handler (unused)
-irqHandler
-        rti
-
-; NMI handler (unused)
-nmiHandler
-        rti
-
-; Initialize RAM and vectors
-        !to $0314
-        .word start        ; Autostart address
-        .word irqHandler   ; IRQ handler address
-        .word nmiHandler   ; NMI handler address
-
-        !to $0801
